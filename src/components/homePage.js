b2c38beab372d1a510c08485bb10641bd6f6df84
JustinH2008 <Justinhawkins2013@gmail.com>
2019-04-02 08:56:14 -0500

    First Commit

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -9,7 +9,7 @@ function HomePage() {
       <p>
         This page is a simple website to function as a Portfolio page as I learn to be a web developer and learn more.
         <br />
-        As I progress in my learning experience and skill set this site will be updated and is subject to change.
+        As I progress in my learning experience and skill set this site will be updated and is subject to change at any time.
         </p>
     </div>
   )
